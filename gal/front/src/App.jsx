9af25ffcefa36bf28d28993e5f8fcdffd8050408
GSIII <kalseo901@gmail.com>
2025-08-19 10:21:20 +0900

    feat : 서류 작성 랜딩페이지 구현

--- gal/front/src/App.jsx
+++ gal/front/src/App.jsx
@@ -1,6 +1,10 @@
 import GithubRepo from "./components/GithubRepo";
 import PortfolioLanding from "./components/PortfolioLanding";
 import PortfolioResult from "./components/PortfolioResult";
+import MetacognitionTestPage from "./page/MetacognitionTestPage";
+import MetacognitionResultPage from "./page/MetacognitionResultPage";
+import ResumePage from "./page/ResumePage";
+import JobStatsPage from "./page/JobStatsPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,8 +13,18 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/github" element={<GithubRepo />} />
-          <Route path="/portfolio-landing" element={<PortfolioLanding />} />
+          <Route path="/document-landing" element={<PortfolioLanding />} />
           <Route path="/portfolio-result" element={<PortfolioResult />} />
+          <Route
+            path="/metacognition-test"
+            element={<MetacognitionTestPage />}
+          />{" "}
+          <Route
+            path="/metacognition-result"
+            element={<MetacognitionResultPage />}
+          />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="/job-stats" element={<JobStatsPage />} />
         </Routes>
       </BrowserRouter>
     </>
